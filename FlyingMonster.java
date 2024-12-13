485cb7f9f194b3beacc58380428e657897667351
daksh1012 <162194794+daksh1012@users.noreply.github.com>
2024-12-12 23:59:51 -0500

    Create FlyingMonster.java

---
+++ FlyingMonster.java
@@ -0,0 +1,29 @@
+public class FlyingMonster extends Monster {
+    private int wingSpan;
+
+    public FlyingMonster(String name, String color, int strength, int speed, int wingSpan) {
+        super(name, color, strength, speed);
+        this.wingSpan = wingSpan;
+    }
+
+    @Override
+    public void performSpecialAbility() {
+        System.out.println(getName() + " is flying with a wingspan of " + wingSpan + " meters!");
+    }
+}
+
+public class AquaticMonster extends Monster {
+    private int swimSpeed;
+
+    public AquaticMonster(String name, String color, int strength, int speed, int swimSpeed) {
+        super(name, color, strength, speed);
+        this.swimSpeed = swimSpeed;
+    }
+
+    @Override
+    public void performSpecialAbility() {
+        System.out.println(getName() + " is swimming at " + swimSpeed + " km/h!");
+    }
+}
+
+// Similar structure for MountainMonster and DesertMonster
